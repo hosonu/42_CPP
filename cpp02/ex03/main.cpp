5282345263d283014210e21bb84fa1d5d95c4843
hosonu <hosonoyuki07@gmail.com>
2024-08-13 21:26:34 +0900

    fin cpp02

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -1,14 +1,18 @@
+#include <iomanip>
 #include "Fixed.hpp"
+#include "Point.hpp"
 
-int main( void ) {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
+bool bsp( Point const a, Point const b, Point const c, Point const point);
+
+int	main() {
+	Point	a(2.02f, 0.83f);
+	Point	b(2.66f, 1.68f);
+	Point	c(1.24f, 1.91f);
+	Point	p(2.07f, 1.46f);
+
+	if (bsp(a, b, c, p) == true)
+		std::cout << "Le point p est dans le triange." << std::endl;
+	else
+		std::cout << "Le point p n'est pas dans le triangle." << std::endl;
 	return 0;
 }
